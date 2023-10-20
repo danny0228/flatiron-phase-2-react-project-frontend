17da70bea23886a241704d990d80300722f962dc
Daniel Tuiran <danny0228@hotmail.com>
2023-10-19 23:41:37 -0400

    imported a component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,11 @@
+import PlayersList from "./PlayersList";
+
 const Main = () => {
     return (
         <>
             <main>
                 <h2> The main section of my app</h2>
-                <p>
-                    cualquier vaina
-                </p>
+                <PlayersList />
             </main>
         </>
     )
